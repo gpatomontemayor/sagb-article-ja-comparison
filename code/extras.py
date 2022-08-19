1b6f04d59d9be42adc345e3122c7a48d67be77d8
Gonzalo Pato Montemayor <100429860@alumnos.uc3m.es>
2022-08-19 22:16:01 +0200

    Final changes

--- code/extras.py
+++ code/extras.py
@@ -1,11 +1,10 @@
+# Class that contains all the information of a player
 class Player:
     def __init__(self, name, nbaid, nbateamid, nbaseasons, bballrefid, bballrefseasons):
         self._name = name
         self._nbaid = nbaid
         self._nbateamid = nbateamid
         self._nbaseasons = nbaseasons
-        self._bballrefid = bballrefid
-        self._bballrefseasons = bballrefseasons
 
     def name(self):
         return self._name
@@ -18,9 +17,3 @@ class Player:
     
     def nbaseasons(self):
         return self._nbaseasons
-
-    def bballrefid(self):
-        return self._bballrefid
-    
-    def bballrefseasons(self):
-        return self._bballrefseasons
